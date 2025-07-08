bd2e3b1cb5b53c9c174378b012ee23916990cc4d
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-07-09 00:15:23 +0300

    chore: add product schema

--- packages/studio/schemaTypes/index.js
+++ packages/studio/schemaTypes/index.js
@@ -1,3 +1,4 @@
 import {home} from './home'
+import {product} from './product'
 
-export const schemaTypes = [home]
+export const schemaTypes = [home, product]
---
+++ packages/studio/schemaTypes/product.js
@@ -0,0 +1,55 @@
+import {defineField, defineType} from 'sanity'
+
+export const product = defineType({
+  name: 'product',
+  title: 'Products',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Product Name',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'desc',
+      title: 'Product Description',
+      type: 'text',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'price',
+      title: 'Product Price',
+      type: 'number',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'images',
+      title: 'Product Images',
+      type: 'array',
+      of: [{type: 'image'}],
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'specs',
+      title: 'Product Specs',
+      type: 'array',
+      of: [
+        {
+          type: 'object',
+          fields: [
+            {
+              name: 'param',
+              type: 'string',
+            },
+            {
+              name: 'value',
+              type: 'string',
+            },
+          ],
+        },
+      ],
+      validation: (rule) => rule.required(),
+    }),
+  ],
+})
