da99f0f412e8042416f26043b2f63edfef4140dc
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-07-10 22:12:12 +0300

    fix: reimport testimonial

--- packages/studio/schemaTypes/index.js
+++ packages/studio/schemaTypes/index.js
@@ -2,6 +2,6 @@ import {categories} from './categories'
 import {genericContent} from './genericContent'
 import {news} from './news'
 import {product} from './product'
-import {testimonial} from './testimonial/'
+import {testimonial} from './testimonial'
 
 export const schemaTypes = [genericContent, product, testimonial, news, categories]
