1d33dd8ecdcc58aa111fe7ecc8afe317ab089be5
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-07-13 15:17:32 +0300

    :fix: fetch obly the first element

--- packages/app/pages/index.vue
+++ packages/app/pages/index.vue
@@ -1,14 +1,14 @@
 <template>
 	<Hero
-		:img="data[0].heroImg"
-		:title="data[0].heroTitle"
-		:tagline="data[0].heroSubtitle"
+		:img="data.heroImg"
+		:title="data.heroTitle"
+		:tagline="data.heroSubtitle"
 		:cta1="{ title: 'Produse', link: '/produse' }"
 		:cta2="{ title: 'Noutăți', link: '/noutati' }"
 	/>
 </template>
 
 <script setup>
-	const query = groq`*[_type == "generic"]{heroTitle,heroSubtitle,"heroImg": heroImg.asset._ref}`;
+	const query = groq`*[_type == "generic"][0]{heroTitle,heroSubtitle,"heroImg": heroImg.asset._ref}`;
 	const { data } = await useSanityQuery(query);
 </script>
