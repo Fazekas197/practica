2eca6fb38d09ae510c83b13129bdc27540d23df1
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-07-13 15:12:56 +0300

    chore: add header

---
+++ packages/app/components/Header.vue
@@ -0,0 +1,93 @@
+<template>
+	<header
+		class="w-full px-6 py-3 lg:px-12 lg:py-6 flex items-center justify-between text-black z-50"
+	>
+		<NuxtLink
+			to="/"
+			class="flex items-center gap-x-3 z-50"
+			@click="navOpen = false"
+		>
+			<slot name="logo"> LOGO </slot>
+		</NuxtLink>
+
+		<button
+			@click="navOpen = !navOpen"
+			aria-label="Toggle navigation"
+			class="relative w-8 h-8 flex flex-col z-50 justify-center items-center gap-1.5 group lg:hidden"
+		>
+			<span
+				:class="[
+					'w-8 h-0.5 bg-black transition-transform duration-300',
+					navOpen ? 'rotate-45 translate-y-2' : '',
+				]"
+			/>
+			<span
+				:class="[
+					'w-8 h-0.5 bg-black transition-opacity duration-300',
+					navOpen ? 'opacity-0' : 'opacity-100',
+				]"
+			/>
+			<span
+				:class="[
+					'w-8 h-0.5 bg-black transition-transform duration-300',
+					navOpen ? '-rotate-45 -translate-y-2' : '',
+				]"
+			/>
+		</button>
+
+		<nav
+			:class="[
+				'text-center fixed inset-y-0 left-0 w-full bg-white flex flex-col justify-center px-6 gap-y-16 transform transition-transform duration-300 ease-out z-40 lg:hidden',
+				navOpen ? 'translate-x-0' : '-translate-x-full',
+			]"
+		>
+			<NuxtLink
+				v-for="link in links"
+				active-class="underline decoration-wavy underline-offset-4"
+				:to="link.link"
+				class="text-4xl"
+				@click="navOpen = false"
+				>{{ link.text }}</NuxtLink
+			>
+		</nav>
+
+		<nav class="text-lg hidden lg:block space-x-6 z-30">
+			<NuxtLink
+				v-for="link in links"
+				:to="link.link"
+				active-class="underline decoration-wavy underline-offset-4"
+				class="hover:text-beige transition decoration-2"
+				>{{ link.text }}</NuxtLink
+			>
+		</nav>
+	</header>
+</template>
+
+<script setup lang="ts">
+	type Link = {
+		text: string;
+		link: string;
+	};
+
+	const props = withDefaults(
+		defineProps<{
+			links?: Link[];
+		}>(),
+		{
+			links: () => [
+				{ text: "link 1", link: "/" },
+				{ text: "link 2", link: "/" },
+			],
+		}
+	);
+
+	const navOpen = ref(false);
+
+	watch(navOpen, (isOpen) => {
+		if (isOpen) {
+			document.body.classList.add("overflow-hidden");
+		} else {
+			document.body.classList.remove("overflow-hidden");
+		}
+	});
+</script>
---
+++ packages/app/layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="overflow-hidden">
+		<Header :links="links" class="absolute bg-white">
+			<template v-slot:logo>
+				<h1 class="text-xl lg:text-2xl">SONOR</h1>
+			</template>
+		</Header>
+		<slot></slot>
+	</div>
+</template>
+
+<script setup>
+	const links = [
+		{ text: "Acasă", link: "/" },
+		{ text: "Produse", link: "/produse" },
+		{ text: "Noutăți", link: "/noutati" },
+		{ text: "Echipă", link: "/echipa" },
+	];
+</script>
