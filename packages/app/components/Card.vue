5e885c42439e28be8a8e27ab5440401014cf9e2d
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-07-13 16:30:04 +0300

    chore: add categorii populare

---
+++ packages/app/components/Card.vue
@@ -0,0 +1,35 @@
+<template>
+	<div
+		class="w-full max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-[20%] bg-white shadow-2xl rounded-3xl flex flex-col overflow-hidden h-full"
+	>
+		<SanityImage :asset-id="img" class="w-full h-48 object-cover" />
+
+		<div class="flex flex-col flex-1 px-4 py-5 space-y-4">
+			<h4 class="text-3xl text-center">{{ title }}</h4>
+			<p class="text-lg flex-1">{{ text }}</p>
+
+			<!-- Button pinned to the bottom -->
+			<UButton
+				v-if="cta"
+				:to="cta.link"
+				class="w-full justify-center mt-auto"
+			>
+				{{ cta.title }}
+			</UButton>
+		</div>
+	</div>
+</template>
+
+<script lang="ts" setup>
+	type CTA = {
+		title: string;
+		link: string;
+	};
+
+	const props = defineProps<{
+		img?: string;
+		title?: string;
+		text?: string;
+		cta?: CTA;
+	}>();
+</script>
--- packages/app/pages/index.vue
+++ packages/app/pages/index.vue
@@ -6,9 +6,34 @@
 		:cta1="{ title: 'Produse', link: '/produse' }"
 		:cta2="{ title: 'Noutăți', link: '/noutati' }"
 	/>
+
+	<div class="my-10">
+		<h2 class="text-4xl xl:text-6xl text-center">Categorii Populare</h2>
+		<div class="xl:flex xl:gap-x-15 justify-center my-10 space-y-10">
+			<Card
+				v-for="cat in data.popularCategories"
+				:img="cat.image"
+				:title="cat.name"
+				:text="cat.desc"
+				:cta="{ title: 'Descoperă', link: `/produse/${cat.name}` }"
+				class="mx-auto xl:m-0"
+			/>
+		</div>
+	</div>
 </template>
 
 <script setup>
-	const query = groq`*[_type == "generic"][0]{heroTitle,heroSubtitle,"heroImg": heroImg.asset._ref}`;
+	const query = groq`
+	*[_type == "generic"][0]{
+		heroTitle,
+		heroSubtitle,
+		"heroImg": heroImg.asset._ref,
+		popularCategories[] -> {
+			name, 
+			desc, 
+			"image": image.asset._ref
+		}
+	}`;
+
 	const { data } = await useSanityQuery(query);
 </script>
