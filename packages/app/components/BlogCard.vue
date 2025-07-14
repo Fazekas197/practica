e4189ef390636b64277a08e210b1410be5afdb67
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-07-14 23:35:23 +0300

    chore: add news page

---
+++ packages/app/components/BlogCard.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="xl:flex w-3/5 mx-auto space-y-6 xl:space-x-12 my-20">
+		<SanityImage :asset-id="img" class="rounded-3xl xl:h-96 object-cover" />
+		<div class="space-y-3 xl:space-y-6">
+			<h2 class="text-2xl xl:text-3xl">{{ title }}</h2>
+			<p class="text-lg xl:text-xl">{{ desc }}</p>
+			<UButton :to="`/noutati/${id}`">Citește</UButton>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+	const props = defineProps<{
+		img?: string;
+		title?: string;
+		desc?: string;
+		id?: string;
+	}>();
+</script>
--- packages/app/pages/noutati/index.vue
+++ packages/app/pages/noutati/index.vue
@@ -1,1 +1,23 @@
-<template></template>
+<template>
+	<div>
+		<BlogCard
+			v-for="news in data"
+			:title="news.title"
+			:img="news.cover"
+			:desc="news.desc"
+			:id="news._id"
+		/>
+		<Newsletter />
+	</div>
+</template>
+
+<script setup>
+	const query = groq`
+	*[_type == "news"]{
+		title,
+    	desc, 
+   	 	"cover": cover.asset._ref,
+        _id
+	}`;
+	const { data } = await useSanityQuery(query);
+</script>
