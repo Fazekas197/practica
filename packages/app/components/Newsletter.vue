499c7fe4a301dbe6c4f1ce0453fb94c5d31ebc73
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-07-14 15:45:31 +0300

    chore: add type for input field

--- packages/app/components/Newsletter.vue
+++ packages/app/components/Newsletter.vue
@@ -4,7 +4,12 @@
 			Abonează-te la Newsletter
 		</h2>
 		<form class="mx-auto space-x-6 xl:w-2/5 flex">
-			<UInput placeholder="Enter your email" size="xl" class="w-full" />
+			<UInput
+				placeholder="Enter your email"
+				size="xl"
+				class="w-full"
+				type="email"
+			/>
 			<UButton class="p-0 !px-4 hover:bg-red/50">Sumbit</UButton>
 		</form>
 	</div>
